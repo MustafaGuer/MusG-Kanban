c8bca5e717c4272b1bde8eca8e84f1bc6a753fab
Mustafa Guer <mustafaguer@outlook.com>
2021-08-25 04:57:49 +0200

    ...

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 git add .
-set /p commit=Enter commit: 
+set /p commit=Enter your Commit: 
 git commit -m "%commit%"
 git push
 
-ng build --prod --base-href="/MusG-Kanban/"
+@REM ng build --prod --base-href="/MusG-Kanban/"
--- git.bat
+++
@@ -1,4 +0,0 @@
-git add .
-set /p commit=Enter commit: 
-git commit -m "%commit%"
-git push
