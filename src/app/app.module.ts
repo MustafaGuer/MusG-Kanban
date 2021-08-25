dd6351d47cdc9368ca25372c6986c9de05dabf8b
Mustafa Guer <mustafaguer@outlook.com>
2021-08-25 06:16:51 +0200

    form-field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
-import { DialogueAddTaskComponent } from './dialogue-add-task/dialogue-add-task.component';
-
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,8 +12,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatInputModule } from '@angular/material/input';
 
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
+import { DialogueAddTaskComponent } from './dialogue-add-task/dialogue-add-task.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,10 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatButtonModule,
     MatDialogModule,
     MatCardModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatFormFieldModule,
+    TextFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
