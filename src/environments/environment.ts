0bcb092d22a7a56d014bfa8d14f0808959e7dc94
Mustafa Guer <mustafaguer@outlook.com>
2021-09-14 23:49:41 +0200

    activated save

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: "AIzaSyCIu_F4Aj7tme62fC_EWUAmASmR7VoMtaI",
-  authDomain: "musg-kanban.firebaseapp.com",
-  projectId: "musg-kanban",
-  storageBucket: "musg-kanban.appspot.com",
-  messagingSenderId: "933969654353",
-  appId: "1:933969654353:web:7f32243f2db5ab0d6abe70"
+    authDomain: "musg-kanban.firebaseapp.com",
+    projectId: "musg-kanban",
+    storageBucket: "musg-kanban.appspot.com",
+    messagingSenderId: "933969654353",
+    appId: "1:933969654353:web:7f32243f2db5ab0d6abe70"
   }
 };
 
